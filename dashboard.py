23ebff9726aeaf054159502d28bd3aa3bb5f3e2d
Cristopher <crisesv4@gmail.com>
2024-11-01 23:00:37 -0600

    Añadir
    Logre añadirlos pero aun no correctamente

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,8 @@
 import flet as ft
 
 def dsh(page):
-    categoria = ft.Dropdown(
+    objects=ft.Column()
+    type = ft.Dropdown(
         hint_text="Categoria",
         options=[
             ft.dropdown.Option(text="Computadoras"), 
@@ -9,31 +10,48 @@ def dsh(page):
             ft.dropdown.Option(text="Robot"),
         ]
     )
+    name=ft.TextField(label="Nombre del dispositivo")
+    watts=ft.TextField(label="Watts", suffix_text="W", keyboard_type=ft.KeyboardType.NUMBER)
     file_picker = ft.FilePicker()
-    page.overlay.append(file_picker)
-    
-    # Controlador (formulario de entrada) - Columna Izquierda
-    controller = ft.Card(
-        content=ft.Container(
-            content=ft.Column(
-                [
-                    ft.Row([ft.TextField(label="Nombre del dispositivo")]),
-                    ft.Row([ft.TextField(label="Watts", suffix_text="W", keyboard_type=ft.KeyboardType.NUMBER)]),
-                    ft.Row([categoria]),
-                    ft.Row([
-                        ft.ElevatedButton(
+    files=ft.ElevatedButton(
                             "Choose files...",
                             on_click=lambda _: file_picker.pick_files(allow_multiple=False, file_type=ft.FilePickerFileType.IMAGE)
                         )
+    page.overlay.append(file_picker)
+    def agregar(e):
+        nombre=name.value
+        wts=watts.value
+        categoria=type.value
+        content=ft.Card(
+            content=ft.Column([
+                ft.Text(f"Nombre: {nombre}"),
+                ft.Text(f"Categoria: {categoria}"),
+                ft.Text(f"Watts: {wts}"),
+                ])
+        )
+        objects.controls.append(content)
+        page.update()
+
+    controller = ft.Row([
+        ft.Card(
+        content=ft.Container(
+            content=ft.Column(
+                [
+                    ft.Row([name]),
+                    ft.Row([watts]),
+                    ft.Row([type]),
+                    ft.Row([
+                        files
                     ]),
-                    ft.Row([ft.ElevatedButton("Guardar", icon=ft.icons.CHECK)]),
+                    ft.Row([ft.ElevatedButton("Guardar", icon=ft.icons.CHECK, on_click=agregar)]),
                 ]
             ),
             padding=25,
             width=400
         )
-    )  
-    dash=ft.c
+    ),
+    objects
+    ])
     page.add(controller)
 
 
