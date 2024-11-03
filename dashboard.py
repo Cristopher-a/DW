781d8c272c88eba0d6ccb27e8c1dd3168a68ea32
Cristopher <crisesv4@gmail.com>
2024-11-02 21:10:11 -0600

    responsive y gridview

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,21 @@
 import flet as ft
 
 def dsh(page):
-    objects=ft.Column()
-    type = ft.Dropdown(
+  
+    is_mobile = page.width < 600  
+    if not is_mobile:
+        page.window.maximized = True
+    
+    grid_view = ft.GridView(
+        expand=False,  
+        max_extent=300 if not is_mobile else 150,  
+        spacing=10, 
+        run_spacing=10,   
+        height=800 if not is_mobile else 400, 
+    )
+    
+    
+    type_dropdown = ft.Dropdown(
         hint_text="Categoria",
         options=[
             ft.dropdown.Option(text="Computadoras"), 
@@ -10,50 +23,50 @@ def dsh(page):
             ft.dropdown.Option(text="Robot"),
         ]
     )
-    name=ft.TextField(label="Nombre del dispositivo")
-    watts=ft.TextField(label="Watts", suffix_text="W", keyboard_type=ft.KeyboardType.NUMBER)
+    name = ft.TextField(label="Nombre del dispositivo")
+    watts = ft.TextField(label="Watts", suffix_text="W", keyboard_type=ft.KeyboardType.NUMBER)
     file_picker = ft.FilePicker()
-    files=ft.ElevatedButton(
-                            "Choose files...",
-                            on_click=lambda _: file_picker.pick_files(allow_multiple=False, file_type=ft.FilePickerFileType.IMAGE)
-                        )
+    files = ft.ElevatedButton(
+        "Choose files...",
+        on_click=lambda _: file_picker.pick_files(allow_multiple=False, file_type=ft.FilePickerFileType.IMAGE)
+    )
     page.overlay.append(file_picker)
+
     def agregar(e):
-        nombre=name.value
-        wts=watts.value
-        categoria=type.value
-        content=ft.Card(
+        nombre = name.value
+        wts = watts.value
+        categoria = type_dropdown.value
+        content = ft.Card(
             content=ft.Column([
                 ft.Text(f"Nombre: {nombre}"),
                 ft.Text(f"Categoria: {categoria}"),
                 ft.Text(f"Watts: {wts}"),
-                ])
+            ])
         )
-        objects.controls.append(content)
+        grid_view.controls.append(content)
         page.update()
 
     controller = ft.Row([
         ft.Card(
-        content=ft.Container(
-            content=ft.Column(
-                [
+            content=ft.Container(
+                content=ft.Column([
                     ft.Row([name]),
                     ft.Row([watts]),
-                    ft.Row([type]),
-                    ft.Row([
-                        files
-                    ]),
+                    ft.Row([type_dropdown]),
+                    ft.Row([files]),
                     ft.Row([ft.ElevatedButton("Guardar", icon=ft.icons.CHECK, on_click=agregar)]),
-                ]
-            ),
-            padding=25,
-            width=400
+                ]),
+                padding=25,
+                width=300 if is_mobile else 400 
+            )
+        ),
+        ft.Container(
+            content=grid_view,
+            width=500 if is_mobile else 1000, 
+            bgcolor="green200"  
         )
-    ),
-    objects
     ])
     page.add(controller)
-
 
 if __name__ == "__main__":
     ft.app(target=dsh)
